b55247bb40664ecfddc13472d1e9180c37055cc8
Stuart Watson <stuart.watson@unep-wcmc.org>
2012-02-20 13:07:55 +0000

    Manual configuration of Postgres added

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,10 +2,24 @@
 # Cookbook Name:: cartodb_github
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, WCMC
 #
 # All rights reserved - Do Not Redistribute
 #
+bash "Install RVM and Ruby" do
+user "ubuntu"
+group "ubuntu"
+code <<-EOF
+mkdir /home/ubuntu/downloads
+cd /home/ubuntu/downloads
+bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
+source ~/.bash_profile
+rvm install 1.9.2
+rvm use 1.9.2
+rvm use 1.9.2 --default
+EOF
+end
 execute "Install CartoDB and dependencies " do
 user "ubuntu"
 group "ubuntu"
@@ -79,6 +93,47 @@ cd /home/ubuntu/downloads
 git clone git://github.com/Vizzuality/Windshaft-cartodb.git
 cd Windshaft-cartodb
 npm install
+sudo pg_dropcluster --stop 9.1 main
+sudo pg_createcluster --start -e UTF-8 9.1 main
+POSTGIS_SQL_PATH='pg_config --sharedir'/contrib/postgis-2.0
+sudo -u postgres createdb -E UTF8 template_postgis
+sudo -u postgres createlang -d template_postgis plpgsql
+sudo -u postgres psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
+sudo -u postgres psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis.sql
+sudo -u postgres psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/spatial_ref_sys.sql
+sudo -u postgres psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/legacy.sql
+sudo -u postgres psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/legacy_compatibility_layer.sql
+sudo -u postgres psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;"
+sudo -u postgres psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+mv ~/.rvm/usr/lib ~/.rvm/usr/lib_rvm
+rvm rvmrc trust "/home/ubuntu/downloads/cartodb/.rvmrc"
+cd /home/ubuntu/downloads/cartodb/
+rvm use 1.9.2@cartodb --create
+/home/ubuntu/.rvm/gems/ruby-1.9.2-p290@global/bin/bundle install --binstubs
+cp /home/ubuntu/downloads/cartodb/config/app_config.yml.sample /home/ubuntu/downloads/cartodb/config/app_config.yml
+cp /home/ubuntu/downloads/cartodb/config/database.yml.sample /home/ubuntu/downloads/cartodb/config/database.yml
+sed -i 's,some_secret,a0be302d0c2b616096974fdf0409a619,g' /home/ubuntu/downloads/cartodb/config/app_config.yml
+sudo ln -s  /usr/lib/postgresql/9.1/bin/shp2pgsql /usr/bin
+sudo rm /etc/postgresql/9.1/main/pg_hba.conf
+sudo touch /etc/postgresql/9.1/main/pg_hba.conf
+sudo sh -c "echo 'local   all             postgres                                trust' >> /etc/postgresql/9.1/main/pg_hba.conf "
+sudo sh -c "echo 'local   all             all                                     trust' >> /etc/postgresql/9.1/main/pg_hba.conf "
+sudo sh -c "echo 'host    all             all             127.0.0.1/32            trust' >> /etc/postgresql/9.1/main/pg_hba.conf "
+sudo sh -c "echo 'host    all             all             ::1/128                 trust' >> /etc/postgresql/9.1/main/pg_hba.conf "
+sudo chown postgres:postgres /etc/postgresql/9.1/main/pg_hba.conf
+sudo /etc/init.d/postgresql restart
+redis-server >> /dev/null 2>&1 &
+cd /home/ubuntu/downloads/Windshaft-cartodb
+node app.js development >> /dev/null 2>&1  &
+cd /home/ubuntu/downloads/CartoDB-SQL-API
+node app.js development >> /dev/null 2>&1  &
+cd /home/ubuntu/downloads/cartodb
+rails s -d
+bundle exec rake cartodb:db:setup EMAIL=username@mysubdomain.com SUBDOMAIN=mysubdomain PASSWORD=password ADMIN_PASSWORD=password
+sudo apt-get update
+sudo apt-get install -y build-essential libopenssl-ruby libcurl4-openssl-dev libssl-dev zlib1g-dev
+sudo sh -c "echo '127.0.0.1 admin.localhost.lan admin.testhost.lan' >> /etc/hosts"
+sudo sh -c "echo '127.0.0.1 my_subdomain.localhost.lan' >> /etc/hosts"
 cd /home/ubuntu/downloads
 wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.21.tar.gz
 tar xvf pcre-8.21.tar.gz
@@ -88,7 +143,7 @@ make
 sudo make install
 sudo apt-get update
 sudo apt-get install -y build-essential libopenssl-ruby libcurl4-openssl-dev libssl-dev zlib1g-dev
-/home/ubuntu/downloads/cartodb/public
+cd /home/ubuntu/downloads/cartodb/public
 gem install passenger
 rvmsudo passenger-install-nginx-module --auto --prefix=/usr/local/nginx --auto-download
 sudo ldconfig
